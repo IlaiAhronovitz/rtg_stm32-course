a3cfaa7410aebb3e6b232588b6bf2c17211183fb
orelorel6 <66217641+orelorel6@users.noreply.github.com>
2025-10-23 16:30:36 +0300

    Add files via upload

---
+++ system_config.h
@@ -0,0 +1,36 @@
+/*
+ * system_config.h
+ *
+ *  Created on: Oct 23, 2025
+ *      Author: orel ezra
+ */
+
+#ifndef INC_SYSTEM_CONFIG_H_
+#define INC_SYSTEM_CONFIG_H_
+#define DAC_EVENT_BIT 0X1
+#define ADC_EVENT_BIT 0X2
+#define DISPLAY_EVENT_BIT 0X4
+#define DAC_MAX_LENGTH_QUUE 64
+#define ADC_MAX_LENGTH_QUUE 64
+#define DISPLAY_MAX_LENGTH_QUUE 256
+#define EVENT_GROUP xEventGroup
+
+
+/*
+#define DAC_HANDLE_QUUE
+#define ADC_HANDLE_QUUE
+#define DISPLAY_HANDLE_QUUE
+#define DAC_MAIN
+*/
+
+
+
+
+
+
+
+
+
+
+
+#endif /* INC_SYSTEM_CONFIG_H_ */
